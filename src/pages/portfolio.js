85067035fc4ee90f40d3823de8c669d5970e6977
Rai Butera <rai@rbutera.com>
2019-07-28 20:46:38 +0100

    added PLYR

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,6 +1,8 @@
+import "../../node_modules/plyr/dist/plyr.css"
+
 import React from "react"
 import { Link } from "gatsby"
-
+import Plyr from "react-plyr"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -9,7 +11,15 @@ const SecondPage = () => (
     <SEO title="Sound Design and Audio Production Portfolio" />
     <h1>Portfolio</h1>
     <p>Check this out:</p>
-
+    <Plyr
+      type="vimeo" // or "vimeo"
+      videoId="148751763"
+      autoplay="true"
+      resetOnEnd="true"
+      clickToPlay="true"
+      autopause="true"
+      quality="1080"
+    />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
