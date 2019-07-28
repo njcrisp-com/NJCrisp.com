8337aff1fa4be9ced17d80163dc0cef04135593c
Rai Butera <rai@rbutera.com>
2019-07-28 22:36:35 +0100

    minor refactor to portfolio

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -7,14 +7,20 @@ import loadable from "@loadable/component"
 
 const Player = loadable(() => import("../components/player"))
 
-const SecondPage = () => (
+const videos = [
+  {
+    videoId: "148751763",
+  },
+]
+
+const Portfolio = () => (
   <Layout>
     <SEO title="Sound Design and Audio Production Portfolio" />
     <h1>Portfolio</h1>
     <p>Check this out:</p>
-    <Player videoId="148751763" />
+    <Player videoId={videos[0].videoId} />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
-export default SecondPage
+export default Portfolio
