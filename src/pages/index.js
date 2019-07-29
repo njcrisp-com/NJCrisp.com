5bc80565c281370fac5fdddc818620891566e239
Rai Butera <rai@rbutera.com>
2019-07-29 16:22:45 +0100

    hooked up homepage to contentful

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,53 @@
 import React from "react"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+
 import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home/About" />
-    <h1>Hi,</h1>
-    <div style={{ maxWidth: `150px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <p>
-      I am <strong>Natan James Crisp,</strong>
-    </p>
-    <p>a Sound Designer from London,</p>
-    <p>and I am passionate about Sound Design and Audio Production.</p>
-    <p>Whilst you are here, why not...</p>
-    <ul>
-      <li>
-        <Link to="/portfolio">Check out my work</Link>
-      </li>
-      <li>
-        <Link to="/tools">Learn about the tools I love to use</Link>
-      </li>
-      <li>
-        <Link to="/contact">Contact me</Link>
-      </li>
-    </ul>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="Home/About" />
+      <div style={{ maxWidth: `150px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+
+      <div id="about-text">
+        {documentToReactComponents(data.page.text.json)}
+      </div>
+      <div id="secondary-navigation">
+        <p>Whilst you are here, why not...</p>
+        <ul>
+          <li>
+            <Link to="/portfolio">Check out my work</Link>
+          </li>
+          <li>
+            <Link to="/tools">Learn about the tools I love to use</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact me</Link>
+          </li>
+        </ul>
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query HomePageQuery {
+    page: contentfulPage(label: { eq: "Home" }) {
+      id
+      label
+      text {
+        id
+        json
+      }
+    }
+  }
+`
 
 export default IndexPage
